4928e352ca4a50a576b9435b895085add12b6617
David Li <jdavidli01@outlook.com>
2016-09-28 21:05:45 -0400

    changes to tea sql

--- drinksTea.sql
+++ drinksTea.sql
@@ -3,78 +3,78 @@ INSERT INTO tea (Name, Price, Description, Image) VALUES
 	"The most frequently drunk and popular variety of green tea in Japan. 
 	The tea leaves are steamed between 15-20 seconds to prevent oxidization. 
 	Then the leaves are rolled and dried to create a thin cylinder like shape.",
-	"" )
+	"http://bit.ly/2dszT5i" )
 INSERT INTO tea (Name, Price, Description, Image) VALUES
 ("Matcha", 2.99, 
 	"Finely ground powdered green tea. The powdered form of matcha is completely
 	consumed as it dissolves in a liquid, usually milk or water. Whipping
 	tea powder and hot water together in a bowl has been a popular method since
 	the 1200's in China.",
-	"" )
+	"http://bit.ly/2cX1I0z" )
 INSERT INTO tea (Name, Price, Description, Image) VALUES
 ("Genmaicha", 2.99, 
 	"50/50 ratio of tea and brown rice, therefore reducing the caffeine content. 
 	Originated in Japan, translated as green tea combined with roasted brown
 	rice. Occasionally referred to as popcorn tea, because of the rice pop in the
 	roasting process. "
-	"" )
+	"http://bit.ly/2cMf1Df" )
 INSERT INTO tea (Name, Price, Description, Image) VALUES
 ("Hojicha", 2.99, 
 	"Clear, light flavour. Roasted at 200 degrees celcius, and then immediately cooled. Has a very
 	distinctive roasting aroma. Roasting trims the caffeine level, which in turn
 	reduces the bitter taste.",
-	"" )
+	"http://bit.ly/2daXFAy" )
 INSERT INTO tea (Name, Price, Description, Image) VALUES
 ("Baihao Yinzhen", 3.99, 
 	"Silver needle, white tea. Prepared with water around 167-176 degree fahrenheit.
 	Flavour is described as sweet and very delicate. Most prized and highest quality 
 	white tea as only the leaf shoots are used to produce the tea.",
-	"" )
+	"http://bit.ly/2dmosXq" )
 INSERT INTO tea (Name, Price, Description, Image) VALUES
 ("Bai Mudan", 2.99, 
 	"White Peony, white tea. Prepared with water around 167-176 degree fahrenheit.
 	Fruity flavour; stronger than Baihao Yinzhen. Pale green and golden, and a
 	fresh, sweet, grassy taste.",
-	"" )
+	"http://bit.ly/2daVlJW" )
 INSERT INTO tea (Name, Price, Description, Image) VALUES
 ("Shou Mei", 2.99, 
 	"A fruity furry white tea, with a mix of tops and leafs. Stronger flavour than
 	Bai Mudan. Similar to oolong teas. Brew time is slightly less then Baihao Yinzhen
 	to maintain the sweetnes and aroma.",
-	"" )
+	"http://bit.ly/2dErqeh" )
 INSERT INTO tea (Name, Price, Description, Image) VALUES
 ("Earl Grey Black Tea", 2.99, 
 	"Earl Grey timeless classic. Citrus flavour and distinctive aroma. Smooth taste
 	infused with bergamot orange from Italy. One of the most popular and traditional 
 	black tea in England.",
-	"" )
+	"http://bit.ly/2dmptin" )
 INSERT INTO tea (Name, Price, Description, Image) VALUES
 ("English Breakfast Tea", 2.99, 
 	"This Royal lineage and morning black tea is a hidden treasure. Time honor classic
 	with the touch of elegance. One of the most popular and traditional black tea in 
 	british tea culture.",
-	"" )
+	"http://bit.ly/2dszKib" )
 INSERT INTO tea (Name, Price, Description, Image) VALUES
 ("Irish Breakfast Tea", 1.99, 
 	"A blend of several black teas. Irish Breakfast tea is commonly served with milk.
 	Being of black tea, it has a strong flavour and much higher caffeine than other 
 	teas. Breakfast tea is misleading, as it is drank as you please.",
-	"" )
+	"http://bit.ly/2dbfnlH" )
 INSERT INTO tea (Name, Price, Description, Image) VALUES
 ("Tie Guan Yin", 1.99, 
 	"Iron Buddha or Iron Goddess of Mercy. One of the most widespread and popular Teas
 	in China. Harvest during the spring and autumn season, to retain the most fruity
 	taste and aroma.",
-	"" )
+	"http://bit.ly/2dlJTLT" )
 INSERT INTO tea (Name, Price, Description, Image) VALUES
 ("Da Hong Pao", 1.99, 
 	"Big Red Robe, is the most famous of the Wuyi rock tea. It is a heavily oxidized
 	dark oolong tea. Many centuries ago, it was known as a cure to an illness. Rich 
 	aromas of roasted vegetables, sweet honey and wet stones.",
-	"" )
+	"http://bit.ly/2duNB78" )http://bit.ly/2dszT5i
 INSERT INTO tea (Name, Price, Description, Image) VALUES
 ("Mi Lan Dan Cong", 1.99, 
 	"Phoenix Honey Orchid, a sweet, honeyed oolong tea is bursting with floral flavour.
 	To stop oxidation the leaves are heated, rolled and baked. Then undergoes a final
 	clean and charcoal bake.",
-	"" )
+	"http://bit.ly/2cMfplj" )
