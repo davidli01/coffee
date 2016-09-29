dfc4357d3a5a9d2968f99d4f7061b1d3df2b8835
Khris Kovoor <Khris Kovoor>
2016-09-29 15:59:15 -0400

    added modal content

---
+++ app.js
@@ -0,0 +1,16 @@
+var coffee = angular.module('app1', []);
+
+coffee.controller('ctrl1', function($scope) {
+
+
+	$scope.coffeeClick = function() {
+		//When clicked it will route to the coffee product page'
+
+	}
+	$scope.teaClick = function() {
+		//When clicked it will route to the tea product page'
+
+		
+	}
+
+});
