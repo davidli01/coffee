16ee47ff2f7bc390f1e36d4b1f435f040d643e80
David Li <jdavidli01@outlook.com>
2016-10-04 15:54:28 -0400

    changes for modal sign in and up

--- modal.js
+++ modal.js
@@ -1,11 +1,11 @@
 // Get the modal
-var modal = document.getElementById('myModal');
+var modal = document.querySelector('modal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var btn = document.querySelector("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.querySelector("close")[0];
 
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
